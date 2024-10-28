c09fda314d919394f88460e17d56b97ca57fda18
devbattery <devbattery@outlook.com>
2024-10-28 18:49:03 +0900

    섹션 3: 쓰레드 로컬 - ThreadLocal
    필드 동기화 - 개발

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,39 @@
+package hello.advanced.trace.logtrace;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @DisplayName("begin end level 2 test")
+    @Test
+    void begin_end_level2() {
+        // given
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        // when
+        // then
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @DisplayName("begin exception level 2 test")
+    @Test
+    void begin_exception_level2() {
+        // given
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        // when
+        // then
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
