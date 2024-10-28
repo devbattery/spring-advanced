6ad5dba4f8c80472f82bdf347010928fdfc95309
devbattery <devbattery@outlook.com>
2024-10-28 19:10:17 +0900

    섹션 3: 쓰레드 로컬 - ThreadLocal
    동시성 문제 - 예제 코드

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,53 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @DisplayName("field test")
+    @Test
+    void field() {
+        // given
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        // when
+        threadA.start();
+//        sleep(100); // 동시성 문제 발생 O
+        sleep(2000); // 동시성 문제 발생 X
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+
+        // then
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
